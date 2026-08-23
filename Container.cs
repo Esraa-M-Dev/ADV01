8e04cb46a25815c3022cb00d72476ea1f018b048
Esraa Mohamed <esomo60@gmail.com>
2026-08-23 13:30:48 +0300

    Q2:create generic class

---
+++ Container.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssigmnentAdvancedC_01
+{
+    internal class Container<T>
+    {
+        private T value;
+        public void Add(T item)
+        {
+            value = item;
+        }
+        public T Get()
+        {
+            return value;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,9 +9,17 @@
             //compiler will resolve type while creating the object
 
             //used to :1-Type safety(compile detection not in run time)
-                    // 2-avoid boxing and un boxing
-                   //  3-resuability and maintainability
-                   //  4-gained control
+            // 2-avoid boxing and un boxing
+            //  3-resuability and maintainability
+            //  4-gained control
+
+            //Q2:Container generic class
+            //Container<int> c1 = new();
+            //c1.Add(5);
+            //Console.WriteLine(c1.Get());
+            //Container<string> c2 = new();
+            //c2.Add("esraa");
+            //Console.WriteLine(c2.Get());
             #endregion
         }
     }
