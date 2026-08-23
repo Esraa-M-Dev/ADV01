0a8ccc4cd3234ee1df9fcb49c08fd403cc105fdd
Esraa Mohamed <esomo60@gmail.com>
2026-08-23 13:07:09 +0300

    Q1:Generic Class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region generic class
+            //Q1:Generic Class:class works with general/different types of its members instead of build more than class
+            //compiler will resolve type while creating the object
+
+            //used to :1-Type safety(compile detection not in run time)
+                    // 2-avoid boxing and un boxing
+                   //  3-resuability and maintainability
+                   //  4-gained control
+            #endregion
         }
     }
 }
